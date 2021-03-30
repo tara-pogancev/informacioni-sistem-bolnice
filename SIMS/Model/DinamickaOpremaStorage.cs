0aec6aa65308e650338bef18232f910e534e484b
ra74-2018 <nikolajovisic4@gmail.com>
2021-03-30 21:54:16 +0200

    Dodata staticka i dinamicka oprema.

---
+++ SIMS/Model/DinamickaOpremaStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class DinamickaOpremaStorage : Storage<string, DinamickaOprema, DinamickaOpremaStorage>
+    {
+        protected override string getPath()
+        {
+            return @".\..\..\..\Data\dinamickaOprema.json";
+        }
+        protected override string getKey(DinamickaOprema dinamickaOprema)
+        {
+            return dinamickaOprema.id;
+        }
+
+        protected override void removeReferences(string key)
+        {
+
+        }
+    }
+}
---
+++ SIMS/Model/StatickaOpremaStorage.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class StatickaOpremaStorage : Storage<string, StatickaOprema, StatickaOpremaStorage>
+    {
+        protected override string getPath()
+        {
+            return @".\..\..\..\Data\statickaOprema.json";
+        }
+        protected override string getKey(StatickaOprema statickaOprema)
+        {
+            return statickaOprema.id;
+        }
+
+        protected override void removeReferences(string key)
+        {
+
+        }
+
+    }
+}
