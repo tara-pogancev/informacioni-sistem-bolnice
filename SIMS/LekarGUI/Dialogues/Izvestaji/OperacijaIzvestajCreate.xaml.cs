e98f45a74e08cf65f79cd66739850f0bba2ea16c
Tara Pogančev <tarapogancev@gmail.com>
2021-05-01 02:52:19 +0200

    CR WPF za izveštaj o operaciji i pisanje terapije

---
+++ SIMS/LekarGUI/Dialogues/Izvestaji/OperacijaIzvestajCreate.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIMS.LekarGUI.Dialogues.Izvestaji
+{
+    /// <summary>
+    /// Interaction logic for OperacijaCreate.xaml
+    /// </summary>
+    public partial class OperacijaIzvestajCreate : Window
+    {
+        public OperacijaIzvestajCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Accept(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ SIMS/LekarGUI/Dialogues/Recepti i terapije/TerapijaPrikaz.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIMS.LekarGUI.Dialogues.Recepti_i_terapije
+{
+    /// <summary>
+    /// Interaction logic for TerapijaPrikaz.xaml
+    /// </summary>
+    public partial class TerapijaPrikaz : Window
+    {
+        public TerapijaPrikaz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
