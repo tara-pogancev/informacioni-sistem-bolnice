e0e8da66ca8544eda83dd65805ef76e4a91181e2
Tara Pogančev <tarapogancev@gmail.com>
2021-05-06 16:53:52 +0200

    Dodatna funkcionalnost za čitanje knjige tokom pauze

--- SIMS/LekarGUI/Dialogues/CoffeeBreak/CoffeeBreakWindow.xaml.cs
+++ SIMS/LekarGUI/Dialogues/CoffeeBreak/CoffeeBreakWindow.xaml.cs
@@ -17,9 +17,77 @@ namespace SIMS.LekarGUI.Dialogues.CoffeeBreak
     /// </summary>
     public partial class CoffeeBreakWindow : Window
     {
+        private int currentPageNumber = 0;
+
         public CoffeeBreakWindow()
         {
             InitializeComponent();
+            LeftArrow.Visibility = Visibility.Hidden;
+        }
+
+        private void PreviousPage(object sender, RoutedEventArgs e)
+        {
+            if (currentPageNumber > 0)
+                PageImage.Source = GetPageByNumber(--currentPageNumber);    
+            
+            if (currentPageNumber == 0)
+                LeftArrow.Visibility = Visibility.Hidden;
+
+            if (currentPageNumber == 6)
+                RightArrow.Visibility = Visibility.Visible;
+        }
+
+        private void NextPage(object sender, RoutedEventArgs e)
+        {
+            if (currentPageNumber < 7)
+                PageImage.Source = GetPageByNumber(++currentPageNumber);
+
+            if (currentPageNumber == 7)
+                RightArrow.Visibility = Visibility.Hidden;
+
+            if (currentPageNumber == 1)
+                LeftArrow.Visibility = Visibility.Visible;
+
+        }
+
+        private BitmapImage GetPageByNumber(int idx)
+        {
+            if (idx == 0)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page0.png", UriKind.Relative));
+            else if (idx == 1)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page1.png", UriKind.Relative));
+            else if (idx == 2)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page2.png", UriKind.Relative));
+            else if (idx == 3)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page3.png", UriKind.Relative));
+            else if (idx == 4)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page4.png", UriKind.Relative));
+            else if (idx == 5)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page5.png", UriKind.Relative));
+            else if (idx == 6)
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page6.png", UriKind.Relative));
+            else
+                return new BitmapImage(new Uri(@"../../../src/CoffeeBreak/page7.png", UriKind.Relative));
+        }
+
+        private void LeftArrowOverlay(object sender, MouseEventArgs e)
+        {
+            LeftArrow.Source = new BitmapImage(new Uri(@"../../../src/CoffeeBreak/arrow_left_overlay.png", UriKind.Relative));
+        }
+
+        private void LeftArrowDefault(object sender, MouseEventArgs e)
+        {
+            LeftArrow.Source = new BitmapImage(new Uri(@"../../../src/CoffeeBreak/arrow_left.png", UriKind.Relative));
+        }
+
+        private void RightArrowOverlay(object sender, MouseEventArgs e)
+        {
+            RightArrow.Source = new BitmapImage(new Uri(@"../../../src/CoffeeBreak/arrow_right_overlay.png", UriKind.Relative));
+        }
+
+        private void RightArrowDefault(object sender, MouseEventArgs e)
+        {
+            RightArrow.Source = new BitmapImage(new Uri(@"../../../src/CoffeeBreak/arrow_right.png", UriKind.Relative));
         }
     }
 }
