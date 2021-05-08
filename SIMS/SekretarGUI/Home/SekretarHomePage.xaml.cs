b1902eeb0859b87ef3db0be536fee2f360d3fca1
MilosZivic75 <zivic.ra75.2018@uns.ac.rs>
2021-05-09 00:16:52 +0200

    Refaktorisanje koda pocetne stranice i glavnog prozora kod sekretara

--- SIMS/SekretarGUI/Home/SekretarHomePage.xaml.cs
+++ SIMS/SekretarGUI/Home/SekretarHomePage.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using Model;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SIMS.SekretarGUI
 {
-    /// <summary>
-    /// Interaction logic for SekretarHomePage.xaml
-    /// </summary>
     public partial class SekretarHomePage : Page
     {
         private static SekretarHomePage instance = null;
--- SIMS/SekretarGUI/SekretarUI.xaml.cs
+++ SIMS/SekretarGUI/SekretarUI.xaml.cs
@@ -1,59 +1,41 @@
 ﻿using Model;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Windows.Threading;
 using SIMS.SekretarGUI;
 using System.Windows.Automation.Peers;
 using System.Windows.Automation.Provider;
 
 namespace SIMS
 {
-
     public partial class SekretarUI : Window
     {
-        private static SekretarUI instance = null;
-        private Sekretar sekretar;
-
-        public static SekretarUI GetInstance()
-        {
-            return instance;
-        }
+        private static SekretarUI _instance = null;
+        private Sekretar _sekretary;
 
         public static SekretarUI GetInstance(Sekretar s)
         {
-            if (instance == null)
-                instance = new SekretarUI(s);
-            return instance;
+            if (_instance == null)
+                _instance = new SekretarUI(s);
+            return _instance;
         }
 
-        private SekretarUI(Sekretar s)
+        private SekretarUI(Sekretar secretary)
         {
             InitializeComponent();
 
-            sekretar = s;
-            UsernameLabel.Content = sekretar.ImePrezime;
+            _sekretary = secretary;
+            UsernameLabel.Content = _sekretary.ImePrezime;
 
-            MainFrame.Content = SekretarHomePage.GetInstance(sekretar);
+            MainFrame.Content = SekretarHomePage.GetInstance(_sekretary);
         }
 
 
-        private void Button_Menu(object sender, RoutedEventArgs e)
+        private void Menu_Click(object sender, RoutedEventArgs e)
         {
             ButtonCloseMenu.Visibility = Visibility.Visible;
         }
 
-        private void Button_Notification(object sender, RoutedEventArgs e)
+        private void Notification_Click(object sender, RoutedEventArgs e)
         {
             if (GridMenu.Width == 320)
             {
@@ -63,28 +45,28 @@ namespace SIMS
                 invokeProv.Invoke();
             }
 
-            MainFrame.Content = new SekretarObavestenjaPage(sekretar);
+            MainFrame.Content = new SekretarObavestenjaPage(_sekretary);
         }
 
-        private void Button_Theme(object sender, RoutedEventArgs e)
+        private void Theme_Click(object sender, RoutedEventArgs e)
         {
             ButtonTheme.Content = ButtonTheme.Content == FindResource("MoonImage") ? FindResource("SunImage") : FindResource("MoonImage");
         }
 
-        private void Button_Language(object sender, RoutedEventArgs e)
+        private void Language_Click(object sender, RoutedEventArgs e)
         {
             ButtonLanguage.Content = (ButtonLanguage.Content.Equals("SR")) ? "EN" : "SR";
         }
 
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        private void CloseMenu_Click(object sender, RoutedEventArgs e)
         {
             ButtonCloseMenu.Visibility = Visibility.Collapsed;
         }
 
-        private void Button_Log_Out(object sender, RoutedEventArgs e)
+        private void LogOut_Click(object sender, RoutedEventArgs e)
         {
             new MainWindow().Show();
-            instance = null;
+            _instance = null;
 
             if (SekretarHomePage.GetInstance() != null)
                 SekretarHomePage.GetInstance().RemoveInstance();
@@ -130,8 +112,6 @@ namespace SIMS
                     invokeProv.Invoke();
                     MainFrame.Content = SekretarHomePage.GetInstance();
                     ListViewMenu.SelectedItem = null;
-                    break;
-                default:
                     break;
             }
         }
